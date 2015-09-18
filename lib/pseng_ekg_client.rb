13d940811704ed243346f335c0527cde01a47355
Cody Tanner <ctanner@instructure.com>
2015-09-17 23:43:48 -0600

    gem works!

--- lib/pseng_ekg_client.rb
+++ lib/pseng_ekg_client.rb
@@ -8,24 +8,29 @@ module PsengEkgClient
     VALID_RECORD_TYPES = %w(application task)
 
     def initialize(base_uri)
-      self.base_uri base_uri
+      self.class.base_uri base_uri
     end
 
     def send_message(record_type, action, payload)
       raise 'Invalid Action' unless VALID_ACTIONS.include?(action)
-      raise 'Invalid Record Type' unless VALID_RECORD_TYPES.include?(action)
+      raise 'Invalid Record Type' unless VALID_RECORD_TYPES.include?(record_type)
+
+      options = {
+        body: payload.to_json,
+        headers: { 'Content-Type' => 'application/json' }
+      }
 
       case action
       when 'index'
-        self.class.get("/#{record_type}s.json", payload)
+        self.class.get("/#{record_type}s.json", options)
       when 'show'
-        self.class.get("/#{record_type}s/#{payload.id}.json", payload)
+        self.class.get("/#{record_type}s/#{payload.id}.json", options)
       when 'create'
-        self.class.post("/#{record_type}s.json", payload)
+        self.class.post("/#{record_type}s.json", options)
       when 'update'
-        self.class.patch("/#{record_type}s/#{payload.id}.json", payload)
+        self.class.patch("/#{record_type}s/#{payload.id}.json", options)
       when 'destroy'
-        self.class.delete("/#{record_type}s/#{payload.id}.json", payload)
+        self.class.delete("/#{record_type}s/#{payload.id}.json", options)
       end
     end
   end
