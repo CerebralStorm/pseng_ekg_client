a71850f787b35e30cc534aef7625d8c8bc7797b9
Cody Tanner <ctanner@instructure.com>
2015-09-22 12:14:51 -0600

    fixes param issue

--- lib/pseng_ekg/client/applications.rb
+++ lib/pseng_ekg/client/applications.rb
@@ -2,24 +2,24 @@ module PsengEkg
   class Client
     module Applications
 
-      def application_index(options = {})
-        self.class.get("/api/v1/applications.json", options)
+      def application_index(params = {})
+        self.class.get("/api/v1/applications.json", query: params)
       end
 
-      def application_show(application_id, options = {})
-        self.class.get("/api/v1/applications/#{application_id}.json", options)
+      def application_show(application_id, params = {})
+        self.class.get("/api/v1/applications/#{application_id}.json", query: params)
       end
 
-      def application_create(options = {})
-        self.class.post("/api/v1/applications.json", options)
+      def application_create(params = {})
+        self.class.post("/api/v1/applications.json", query: params)
       end
 
-      def application_update(applictation_id, options = {})
-        self.class.patch("/api/v1/applications/#{application_id}.json", options)
+      def application_update(applictation_id, params = {})
+        self.class.patch("/api/v1/applications/#{application_id}.json", query: params)
       end
 
-      def application_destroy(applictation_id, options = {})
-        self.class.destroy("/api/v1/applications.json", options)
+      def application_destroy(applictation_id, params = {})
+        self.class.destroy("/api/v1/applications.json", query: params)
       end
 
     end
--- lib/pseng_ekg/client/tasks.rb
+++ lib/pseng_ekg/client/tasks.rb
@@ -2,24 +2,24 @@ module PsengEkg
   class Client
     module Tasks
 
-      def task_index(application_id, options = {})
-        self.class.get("/api/v1/applications/#{application_id}/tasks.json", options)
+      def task_index(application_id, params = {})
+        self.class.get("/api/v1/applications/#{application_id}/tasks.json", query: params)
       end
 
-      def task_show(application_id, task_id, options = {})
-        self.class.get("/api/v1/applications/#{application_id}/tasks/#{task_id}.json", options)
+      def task_show(application_id, task_id, params = {})
+        self.class.get("/api/v1/applications/#{application_id}/tasks/#{task_id}.json", query: params)
       end
 
-      def task_create(application_id, options = {})
-        self.class.post("/api/v1/applications/#{application_id}/tasks.json", options)
+      def task_create(application_id, params = {})
+        self.class.post("/api/v1/applications/#{application_id}/tasks.json", query: params)
       end
 
-      def task_update(applictation_id, task_id, options = {})
-        self.class.patch("/api/v1/applications/#{application_id}/tasks/#{task_id}.json", options)
+      def task_update(applictation_id, task_id, params = {})
+        self.class.patch("/api/v1/applications/#{application_id}/tasks/#{task_id}.json", query: params)
       end
 
-      def task_destroy(applictation_id, task_id, options = {})
-        self.class.destroy("/api/v1/applications/#{application_id}/tasks/#{task_id}.json", options)
+      def task_destroy(applictation_id, task_id, params = {})
+        self.class.destroy("/api/v1/applications/#{application_id}/tasks/#{task_id}.json", query: params)
       end
 
     end
